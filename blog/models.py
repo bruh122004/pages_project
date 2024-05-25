90f7c3e12a4bb79394eb500c4d23114851aead23
idir <idirbadache16@gmail.com>
2024-05-25 17:54:30 +0100

    forms

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 #importin the class models and creating a sub class called Post
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +9,7 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
+    
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import request
 # Create your views here.
-
-from django.views.generic import ListView, DetailView, CreateView
-
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
 
 class BlogListView(ListView):
@@ -17,3 +22,12 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+    
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
